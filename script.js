2bfa44f56da9d714270ab7f86fd28d3d7d8c1437
Thamarai <thamaraiselvig2109@gmail.com>
2023-07-24 12:50:50 +0530

    new project

---
+++ script.js
@@ -0,0 +1,45 @@
+
+function para1(){
+  document.getElementById("para1").innerHTML =
+    "Email campaigns have one ability that other channels dont. It creates valuable personal touches at scale.";
+}
+function para2() {
+  document.getElementById("para2").innerHTML =
+    "Email campaigns have one ability that other channels dont. It creates valuable personal touches at scale.";
+}
+function para3() {
+  document.getElementById("para3").innerHTML =
+    "Email campaigns have one ability that other channels dont. It creates valuable personal touches at scale.";
+}
+function para4() {
+  document.getElementById("para4").innerHTML =
+    "Email campaigns have one ability that other channels dont. It creates valuable personal touches at scale.";
+}
+function para5() {
+  document.getElementById("para5").innerHTML =
+    "Email campaigns have one ability that other channels dont. It creates valuable personal touches at scale.";
+}
+function para6() {
+  document.getElementById("para6").innerHTML =
+    "Email campaigns have one ability that other channels dont. It creates valuable personal touches at scale.";
+}
+function paralv1() {
+  document.getElementById("para1").innerHTML = "";
+}
+function paralv2() {
+  document.getElementById("para2").innerHTML = "";
+}
+function paralv3() {
+  document.getElementById("para3").innerHTML = "";
+}
+function paralv4() {
+  document.getElementById("para4").innerHTML = "";
+}
+function paralv5() {
+  document.getElementById("para5").innerHTML = "";
+}
+function paralv6() {
+  document.getElementById("para6").innerHTML = "";
+}
+
+
